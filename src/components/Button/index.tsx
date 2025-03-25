fc175f1f50f968272122acb4748edfa794222df5
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-03-25 14:00:07 -0300

    feat: start project

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function Button(): JSX.Element {
+    return (
+        <div>
+            <button>Click me</button>
+        </div>
+    );
+}
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,10 @@
+import React, { JSX } from 'react';
+
+export default function Form(): JSX.Element {
+    return (
+        <form>
+            <input type="text"></input>
+            <button>Submit</button>
+        </form>
+    );
+}
